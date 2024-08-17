c463558a2a05c14e5aeb2afe9d8fb126fbe3a1fd
MJMac <minjun@gimminjun-ui-MacBookAir.local>
2024-08-17 12:35:14 +0900

    Initial commit

---
+++ src/main/java/com/example/travel_on/TravelOnApplication.java
@@ -0,0 +1,13 @@
+package com.example.travel_on;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelOnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TravelOnApplication.class, args);
+    }
+
+}
